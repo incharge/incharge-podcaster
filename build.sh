2c0472ac557959508549ce78d4341e607f3e7588
Julian Price <github.com@julianprice.org.uk>
2024-03-07 15:04:09 +0000

    Copy posts before building

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
+# Use the 'withastro' action to build on GitHub and deploy on GitHub Pages https://github.com/withastro/action
+# Alternatively run this script.
+# It assumes npm is already installed.
+#!/usr/bin/env bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+mkdir -p astrowind/src/content/post
+cp md/*.md astrowind/src/content/post/  
 cd "$SCRIPT_DIR/../astrowind"
 npm run build
